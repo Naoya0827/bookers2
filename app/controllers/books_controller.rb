d72299b70680274dfbbe2659c1e054383e017e81
Naoya0827 <n.freedom.yazuna@gmail.com>
2021-11-29 16:27:50 +0000

    [Fix]5-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
 
  def create
-  @book = Book.new(book_params)
-  @book.user_id = current_user.id
-  if @book.save
+  @newbook = Book.new(book_params)
+  @newbook.user_id = current_user.id
+  if @newbook.save
      flash[:notice]="You have creatad book successfully."
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@newbook.id)
   else
    @user = current_user
    @books =Book.all
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
  def index
   @books = Book.all
   @user = current_user
-  @book = Book.new
+  @newbook = Book.new
  end
 
  def edit
@@ -49,6 +49,10 @@ class BooksController < ApplicationController
   @book = Book.find(params[:id])
   @book.destroy
   redirect_to books_path
+ end
+
+ def newbook
+  @newbook = Book.new(book_params)
  end
 
  private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = Book.new
+    @newbook = Book.new
     @books = Book.where(user_id: @user.id)
   end
 
   def index
     @user = current_user
     @users = User.all
-    @book = Book.new
+    @newbook = Book.new
     @books = Book.all
   end
 
